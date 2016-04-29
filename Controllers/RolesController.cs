2ce353c463532b33ffe989f54545ecde31f45f54
lawtonb <lawton.a.browning@gmail.com>
2016-04-29 11:32:27 -0700

    add create role to database, list roles on page

---
+++ Controllers/RolesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Http.Internal;
+using PcPdx.Models;
+using Microsoft.Data.Entity;
+using System.Diagnostics;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Authorization;
+
+namespace PcPdx.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public RolesController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = _db.Roles.ToList();
+            return View(roles);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string RoleName)
+        {
+            try
+            {
+                _db.Roles.Add(new IdentityRole()
+                {
+                    Name = RoleName
+                });
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                return View();
+            }
+        }
+    }
+}
